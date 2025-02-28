d0ec5baf4116c314173d9cdf104c36bb14a12b4e
Damaris Ngari <wanjirungari2@gmail.com>
2025-02-28 03:31:41 +0300

    create.sql

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ('Mr. Chocolate', 20, 'M', 'dark brown', 'calm', 1);
+INSERT INTO bears (name,  age, sex, color, temperament, alive) VALUES ('Rowdy', 10, 'M', 'black', 'noisy', 1);
+INSERT INTO bears (name,  age, sex, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'dark brown', 'sedate', 1);
+INSERT INTO bears (name,  age, sex, color, temperament, alive) VALUES ('Sargeant Brown', 10, 'M', 'green', 'loud', 1);
+INSERT INTO bears (name,  age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'F', 'purple', 'mellow', 1);
+INSERT INTO bears (name,  age, sex, color, temperament, alive) VALUES ('Grinch', 2, 'M', 'blue', 'sneaky', 1);
+INSERT INTO bears (name,  age, sex, color, temperament, alive) VALUES ('Wendy', 6, 'F', 'pink', 'sweet', 1); 
+INSERT INTO bears (name,  age, sex, color, temperament, alive) VALUES (NULL, 8, 'F', 'green', 'energetic', 1);
